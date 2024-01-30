9cee6e17cf59088a8dc22458e97c749439e19355
nando <nando@hex7.com>
2024-01-31 00:54:42 +0100

    scores

---
+++ upload_scores.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CONTAINER=$(docker ps --latest --format {{.ID}})
+DATE=$(date +%Y-%m-%d)
+mkdir -pv tmp/data
+docker cp "${CONTAINER}:/data/scores-${DATE}.json" "tmp/data/"
+aws s3 cp ./tmp/data/scores-${DATE}.json s3://${S3_BUCKET}/scores-${DATE}.json
