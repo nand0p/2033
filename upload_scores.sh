0847bf1c9f3b63a4d95960347c95b3e415c44e19
nando <nando@hex7.com>
2024-01-31 01:21:21 +0100

    scores

--- upload_scores.sh
+++ upload_scores.sh
@@ -10,7 +10,5 @@ echo $CONTAINER
 DATE=$(date +%Y-%m-%d)
 echo $DATE
 mkdir -pv tmp/data
-find ./data/
 docker cp "${CONTAINER}:/data/scores-${DATE}.json" "tmp/data/"
 find ./tmp/
-#aws s3 cp ./tmp/data/scores-${DATE}.json s3://${S3_BUCKET}/scores-${DATE}.json
