0bbabe58d765216b97d68adb33472d006ba8d4e6
nando <nando@hex7.com>
2024-01-31 01:14:04 +0100

    test

--- upload_scores.sh
+++ upload_scores.sh
@@ -6,7 +6,11 @@ which mkdir
 which aws
 
 CONTAINER=$(docker ps --latest --format {{.ID}})
+echo $CONTAINER
 DATE=$(date +%Y-%m-%d)
+echo $DATE
 mkdir -pv tmp/data
+find ./data/
 docker cp "${CONTAINER}:/data/scores-${DATE}.json" "tmp/data/"
-aws s3 cp ./tmp/data/scores-${DATE}.json s3://${S3_BUCKET}/scores-${DATE}.json
+find ./tmp/
+#aws s3 cp ./tmp/data/scores-${DATE}.json s3://${S3_BUCKET}/scores-${DATE}.json
