6a0ab10ab3468c2aaa98387655fe01fd5bbaa7eb
nando <nando@hex7.com>
2024-02-22 03:02:38 +0100

    types

--- utils/stocks.py
+++ utils/stocks.py
@@ -9,5 +9,19 @@ def get_stocks(stocks, cat):
     if v == cat or cat == '0':
       r[s] = {}
       r[s]['category'] = v
+      if v == '1':
+        r[s]['catname'] = 'Cloud XL'
+      elif v == '2':
+        r[s]['catname'] = 'Cloud Stock'
+      elif v == '3':
+        r[s]['catname'] = 'ETF'
+      elif v == '4':
+        r[s]['catname'] = 'BioTech'
+      elif v == '5':
+        r[s]['catname'] = 'Sustainable Future'
+      elif v == '6':
+        r[s]['catname'] = 'Crypto'
+      else:
+        r[s]['catname'] = 'None'
 
   return r
