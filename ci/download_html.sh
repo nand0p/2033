34b5074f531f4f16c4d01814597b1c2a5f3fb460
nando <nando@hex7.com>
2024-03-08 15:28:42 +0100

    html

--- ci/download_html.sh
+++ ci/download_html.sh
@@ -1,5 +1,26 @@
-#!/bin/bash
+#!/bin/bash -ex
 
+
+echo download top level html
+for SPEED in 'fast' 'slow'; do
+  pwd
+  echo execute ${SPEED}
+  mkdir -pv ${SPEED}
+  cd ${SPEED}
+  wget --recursive \
+       --no-directories \
+       --no-host-directories \
+       --adjust-extension \
+       --no-cache \
+       --no-cookies \
+       --directory-prefix=${SPEED} \
+       --default-page=index.html \
+       "localhost?speed=${SPEED}"
+  cd ..
+  sleep 1
+done
+
+echo download sub-site html
 for SPEED in 'fast' 'slow'; do
   for BASE in 1 2 3 4 5 6 7 8; do
     pwd
