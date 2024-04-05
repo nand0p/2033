f6fa50724de92a3130bf92a6c2d95b1808c81dd3
nando <nando@hex7.com>
2024-04-05 03:50:11 +0200

    wrapper

--- trade/get_partial_share_trades.sh
+++ trade/get_partial_share_trades.sh
@@ -6,10 +6,13 @@ echo
 echo auto-generate all possible buy json
 echo
 
-for CATEGORY in $(seq 8); do
-  echo
-  echo CATEGORY ${CATEGORY}
-  echo "python get_partial_share_trades.py --category ${CATEGORY} --all"
-  python get_partial_share_trades.py --category ${CATEGORY} --all
-  sleep 1
+for SPEED in "--fast" "--slow"; do
+  for CATEGORY in $(seq 8); do
+    echo
+    echo CATEGORY ${CATEGORY}
+    echo SPEED ${SPEED}
+    echo "python get_partial_share_trades.py ${SPEED} --category ${CATEGORY} --all"
+    python get_partial_share_trades.py ${SPEED} --category ${CATEGORY} --all
+    sleep 1
+  done
 done
