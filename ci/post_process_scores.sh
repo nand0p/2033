6d441f2fc4b0a7481dea1a8884f0d8ebdd31fef0
nando <nando@hex7.com>
2024-04-07 00:10:42 +0200

    fix

--- ci/post_process_scores.sh
+++ ci/post_process_scores.sh
@@ -4,18 +4,17 @@
 HTML=1
 DOWNLOAD=0
 UPLOAD=0
-DEBUG=0
+DEBUG=1
 
 
 SOURCE_BUCKET=2030.hex7.com
 DEST_BUCKET=2033.hex7.com
-S3_PREFIX="scores"
+S3_PREFIX="scores/"
 TMP_DIR="./tmp"
 mkdir -pv ${TMP_DIR}
 
 
-SCORES_LIST=$(aws s3 ls s3://${DEST_BUCKET}/${S3_PREFIX}/ | cut -d' ' -f9)
-#SCORES_LIST=$(aws s3 ls s3://${SOURCE_BUCKET}/${S3_PREFIX}/ | cut -d' ' -f9)
+SCORES_LIST=$(aws s3api list-objects --bucket ${DEST_BUCKET} --prefix ${S3_PREFIX} --query Contents[*].Key --output text)
 
 
 echo
@@ -43,29 +42,31 @@ fi
 
 
 for SCORES_FILE in ${SCORES_LIST}; do
-  echo "====> SCORES_FILE: ${SCORES_FILE}"
+  if [[ "${SCORES_FILE}" != "scores/index.html" ]]; then
+    echo "====> SCORES_FILE: ${SCORES_FILE}"
 
-  if [[ "${DOWNLOAD}" == "1" ]]; then
-    aws s3 cp s3://${SOURCE_BUCKET}/${S3_PREFIX}/${SCORES_FILE} ${TMP_DIR}/${SCORES_FILE}
-    echo "======> copy from source success"
+    if [[ "${DOWNLOAD}" == "1" ]]; then
+      aws s3 cp s3://${SOURCE_BUCKET}/${SCORES_FILE} ${TMP_DIR}/${SCORES_FILE}
+      echo "======> copy from source success"
+    fi
+
+    if [[ "${UPLOAD}" == "1" ]]; then
+      aws s3 cp --acl public-read ${TMP_DIR}/${SCORES_FILE} s3://${DEST_BUCKET}/${SCORES_FILE}
+      echo "======> copy to dest success"
+    fi
+
+    if [[ "${HTML}" == "1" ]]; then
+      echo "<a href=http://${DEST_BUCKET}/${SCORES_FILE}>${SCORES_FILE}</a><br>" |tee -a ${TMP_DIR}/index.html >/dev/null
+      echo "======> generate html success"
+    fi
+
   fi
-
-  if [[ "${UPLOAD}" == "1" ]]; then
-    aws s3 cp --acl public-read ${TMP_DIR}/${SCORES_FILE} s3://${DEST_BUCKET}/${S3_PREFIX}/${SCORES_FILE}
-    echo "======> copy to dest success"
-  fi
-
-  if [[ "${HTML}" == "1" ]]; then
-    echo "<a href=http://${DEST_BUCKET}/${S3_PREFIX}/${SCORES_FILE}>${SCORES_FILE}</a><br>" |tee -a ${TMP_DIR}/index.html >/dev/null
-    echo "======> generate html success"
-  fi
-
 done
 
 
 if [[ "${HTML}" == "1" ]]; then
   echo "</body></html>" | tee -a ${TMP_DIR}/index.html >/dev/null
-  aws s3 cp --acl public-read ${TMP_DIR}/index.html s3://${DEST_BUCKET}/${S3_PREFIX}/index.html
+  aws s3 cp --acl public-read ${TMP_DIR}/index.html s3://${DEST_BUCKET}/${S3_PREFIX}
 fi
 
 
